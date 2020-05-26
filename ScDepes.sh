7ce2bab5c61df5039eb99c7d427d9d73fa3faa4c
Kanchil-ID <65784627+kanchil08@users.noreply.github.com>
2020-05-26 23:30:14 +0700

    Create ScDepes.sh

---
+++ ScDepes.sh
@@ -0,0 +1,241 @@
+#!/system/xbin/bash
+#Jangan di recode dong ster
+#Hargai Mereka Yang Ngoding
+#Karna Ngoding gk segamapang dapetin cinta kamu
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo $cyan "Terkadang kita Hidup lupa akan Tanah"
+sleep 2
+echo $blue "Terkadang kita Ini Lupa Kita Itu Siapa"
+sleep 2
+echo $green "Terkadang kita Sombong Dengan Yang Kita Punya"
+sleep 2
+echo $cyan "Terkadang kita Ini Lupa Kita Hanya Manusia"
+sleep 2
+echo $blue "Terkadang kita Lupa Kita Hanya Sebutir Pasir Di Mata Allah"
+sleep 2
+echo $green "Terkadang kita Ini Lupa Manusia Ciptaan Allah"
+sleep 2
+echo $cyan "Lantas Apa Yang Perlu Di Sombomgkan Di Dunia Ini"
+sleep 2
+echo $blue "Dan Mohon Kalau Recode Jangan Hilangkan Hak Cipta"
+sleep 2
+echo $green "Karena Ngoding Tak Semudah Mendapatkan Cinta Kamu"
+sleep 4
+echo $cyan "Ttd Tegar Gans:v"
+sleep 1
+clear
+echo ""
+
+echo "\033[0;32m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(oo)/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
++==========UU====UU=================+
++          *//||\\*                 +
++           ****                    +
++ coded by : Kanchil-ID             +
++ Team : Official Manusia Tersakiti +
++ kontak me : 6282125068665         +
++===================================+
+  ^...^                   ^...^
+ <_* *_>Shell&ScriptDepes<_* *_>
+   \_/                     \_/   ";
+echo $blue "-=[MenuShell]=-"
+echo $green "[====================================[>"
+echo "1.alfa"|lolcat
+echo $red "[====================================[>"
+echo "2.IndoXploit"|lolcat
+echo $yellow "[====================================[>"
+echo "3.extazy"|lolcat
+echo $green "[====================================[>"
+echo "4.kojek"|lolcat
+echo $yellow "[====================================[>"
+echo "5.Ani-Shell"|lolcat
+echo $yellow "[====================================[>"
+echo "6.tatsumicrew"|lolcat
+echo $yellow "[====================================[>"
+echo "7.b374k"|lolcat
+echo $green "[====================================[>"
+echo "-=[MenuScriptDefaceKeren]=-"|lolcat
+echo $blue "[====================================[>"
+echo "8.Anime"|lolcat
+echo $yellow "[====================================[>"
+echo "9.SimpleTapiKeren"|lolcat
+echo $cyan "[====================================[>"
+echo "10.GifNyaKeren"|lolcat
+echo $yellow "[====================================[>"
+echo "11.Bintang-Bintang"|lolcat
+echo $purple "[====================================[>"
+echo "12.Vj15"|lolcat
+echo $red "[====================================[>"
+echo "13.UlangTahun"|lolcat
+echo $yellow "[====================================[>"
+echo "14.WarnaWarni"|lolcat
+echo $green "[====================================[>"
+echo "00."|lolcat
+echo $red
+read -p "[>>" gmt
+
+if [ $gmt = 1 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f alfa.php /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 2 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f indoxploit.php /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 3 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f extazy.php /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 4 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f kojek.php /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+echo $blue "Pass nya : kenatipucok"
+fi
+
+if [ $gmt = 5 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f Ani-Shell.php /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 6 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f Tatsumicrew.php /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+echo $blue "Pass nya : tatsumi2k18"|lolcat
+fi
+
+if [ $gmt = 7 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f b374k-2.2.php /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 8 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f e.html /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 9 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f ahha.html /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 10 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f WeakYour.html /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 11 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f TYS.html /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 12 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f vj15.html /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 13 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f Ultah.html /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 14 ]
+then
+clear
+echo "Wait Bro"
+cd menu
+mv -f LookTechCorp.html /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 00 ]
+then
+clear
+echo "ThanksTo : Look-T - Wannabe - extazy - Vijune15 - TuanSwam"|lolcat
+echo "Kalo Ingin Direcode silahkan"|lolcat
+echo "Hak cipta Jangan Dirubah Hargai mereka coding gan"|lolcat
+echo "Ngecoding Gk Segampang Dapetin Cinta Kamu:)"|lolcat
+fi
